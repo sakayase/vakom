f418981776eaad462567c072cdc2c03b25b1d2b5
sakayase <sakayase>
2021-02-01 18:20:09 +0100

    Formulaire d'authentification Page de login Sécurisation User (todo commentaire & article)

--- vakom-back/src/Form/UserType.php
+++ vakom-back/src/Form/UserType.php
@@ -5,7 +5,12 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class UserType extends AbstractType
 {
@@ -13,9 +18,37 @@ class UserType extends AbstractType
     {
         $builder
             ->add('email')
-            ->add('roles')
-            ->add('password')
-        ;
+            ->add('roles', ChoiceType::class, [
+                'choices' => [
+                    'administrateur' => 'ROLE_ADMIN',
+                    'utilisateur' => 'ROLE_USER',    
+                    ],
+                'multiple' => true,
+                'expanded' => true,
+                ])
+            ->add('plainPassword', RepeatedType::class, [
+                // instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                'mapped' => false,
+                'type' => PasswordType::class,
+                'invalid_message' => 'The password fields must match.',
+                'options' => ['attr' => ['class' => 'password-field']],
+                'required' => true,
+                'first_options'  => ['label' => 'Password'],
+                'second_options' => ['label' => 'Repeat Password'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a password',
+                    ]),
+                    // new Length([
+                    //     'min' => 6,
+                    //     'minMessage' => 'Your password should be at least {{ limit }} characters',
+                    //     // max length allowed by Symfony for security reasons
+                    //     'max' => 4096,
+                    // ])
+                ]
+            ]
+        );
     }
 
     public function configureOptions(OptionsResolver $resolver)
