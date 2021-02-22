a0e0f85e267b82d4360fa1d40775e526699e0804
sakayase <sakayase>
2021-02-22 17:50:38 +0100

    config vue/symfony sur webpack-encore

--- vakom-back/assets/app.js
+++ vakom-back/assets/app.js
@@ -1,12 +1,8 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
+import Vue from 'vue'
 
-// any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
-
-// start the Stimulus application
-import './bootstrap';
+new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!'
+    }
+})
